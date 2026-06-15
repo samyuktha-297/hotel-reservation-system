81a27625021d3f869a02a67880a703c214c1e30a
samyuktha-297 <samyuktharanjithkumar@gmail.com>
2026-06-15 11:54:23 +0530

    Application Init

---
+++ hrs/src/test/java/my/personal/hrs/HrsApplicationTests.java
@@ -0,0 +1,13 @@
+package my.personal.hrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
